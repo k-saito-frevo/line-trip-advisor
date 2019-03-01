85a8b975f4b24ea4819b409a2bd1ec3c5c2ec29a
k-saito-frevo <k.saito@frevo-works.co.jp>
2019-03-01 16:16:16 +0900

    imgテスト

--- src/main/java/com/linetripadvisor/linetripadvisor/MessageHandler.java
+++ src/main/java/com/linetripadvisor/linetripadvisor/MessageHandler.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -30,6 +33,14 @@ public class MessageHandler {
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
         System.out.println("event: " + event);
         System.out.println("Sent messages: " + event.getMessage().getText());
+    }
+    
+    @EventMapping
+    public TextMessage handleImageMessageEvent(MessageEvent<ImageMessageContent>event) {
+    	Object receiveImage = event.getSource();
+    	
+//    	BotApiResponse response = replyMessageHandler.reply(event);
+    	return new TextMessage("お返ししま〜す");
     }
 
     @EventMapping
