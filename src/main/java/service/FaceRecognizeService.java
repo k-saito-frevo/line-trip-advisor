c5a9cdbfa74a635f6d5ae54056e772888980dc8e
k-saito-frevo <k.saito@frevo-works.co.jp>
2019-03-26 16:56:50 +0900

    メッセージIDを変更

--- src/main/java/service/FaceRecognizeService.java
+++ src/main/java/service/FaceRecognizeService.java
@@ -8,17 +8,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpEntity;	
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -208,9 +204,9 @@ import model.FaceDetail;
 					FaceRecognizeService frs = new FaceRecognizeService();
 					res=frs.generateLocation(targetArr);
 				}
-				return "OK";
+				return res;
 			}catch(Exception ex){
-				return "エラー";
+				return "エラーが発生しました。しばらく経った後再度お送りください。";
 			}
 		}
 		//
