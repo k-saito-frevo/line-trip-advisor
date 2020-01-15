75d7dbf933a4dbeae67e8a96551e207be683a2dc
frevoworks <k.saito@frevo-works.co.jp>
2020-01-15 18:03:42 +0900

    autowired修正

--- src/main/java/com/linetripadvisor/linetripadvisor/MessageHandler.java
+++ src/main/java/com/linetripadvisor/linetripadvisor/MessageHandler.java
@@ -54,10 +54,7 @@ public class MessageHandler {
     private LineMessagingClient lineMessagingClient;
 	@Autowired
 	ResourceLoader resourceLoader;
-	@Autowired
-	FaceRecognizeService faceRecognizeService;
-	@Autowired
-	ContentService contentService;
+	
 	
 	//テキストメッセージがきた時に呼ばれるよ
     @EventMapping
@@ -71,6 +68,7 @@ public class MessageHandler {
     	try {
     		System.out.println("とりあえず届いてるよー　");
 	    	String messageId = event.getMessage().getId();
+	    	ContentService contentService = new ContentService();
 			Date date = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");        
 			String jpegTarget = "./line-test" + sdf.format(date).toString() + ".jpg";
@@ -78,6 +76,7 @@ public class MessageHandler {
 	    	String imgStr = contentService.getContent(messageId,jpegTarget);
 	    	if(imgStr.isEmpty() || imgStr == null) return new TextMessage("エラーです");
 	    	//顔認証取得
+	    	FaceRecognizeService faceRecognizeService = new FaceRecognizeService();
 	    	String result = faceRecognizeService.tryPost(imgStr);
 	    	System.out.println("＊＊＊＊＊リザルト＊＊＊＊＊");
 	    	System.out.println(result);
